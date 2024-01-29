ee7dffeee560b45960370be9615c8adf14d18073
Mashhoor Ahdal <101206478+mashhoorahdal@users.noreply.github.com>
2024-01-29 15:18:01 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
     // all the properties are optional - can be left empty or deleted
-    homepage: 'https://mashhoor-ahdal.github.io/portfolio',
+    homepage: 'https://mashhoorahdal.github.io/portfolio',
     title: 'MA.',
   }
   
