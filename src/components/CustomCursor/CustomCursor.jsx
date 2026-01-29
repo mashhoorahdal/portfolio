d6d8f10f436e42686f89b8c44fa9c445ea2b73f0
mashhoor.ahdal <mashhoor.ahdal@scrapehero.com>
2026-01-30 01:15:43 +0530

    Refactor CustomCursor component to improve visibility logic and event handling

--- src/components/CustomCursor/CustomCursor.jsx
+++ src/components/CustomCursor/CustomCursor.jsx
@@ -1,52 +1,48 @@
 // CustomCursor.js
-
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./CustomCursor.css";
 
 const CustomCursor = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [isVisible, setIsVisible] = useState(true);
-
-  const updateCursorPosition = (e) => {
-    setPosition({ x: e.clientX, y: e.clientY });
-  };
-
-  const handleMouseEnter = () => {
-    setIsVisible(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsVisible(false);
-  };
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
+    const updateCursorPosition = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+      if (!isVisible) setIsVisible(true);
+    };
+
+    const handleMouseLeave = () => {
+      setIsVisible(false);
+    };
+
+    const handleMouseEnter = () => {
+      setIsVisible(true);
+    };
+
     document.addEventListener("mousemove", updateCursorPosition);
-    window.addEventListener("mouseenter", handleMouseEnter);
-    window.addEventListener("mouseleave", handleMouseLeave);
+    document.documentElement.addEventListener("mouseleave", handleMouseLeave);
+    document.documentElement.addEventListener("mouseenter", handleMouseEnter);
 
     return () => {
       document.removeEventListener("mousemove", updateCursorPosition);
-      window.removeEventListener("mouseenter", handleMouseEnter);
-      window.removeEventListener("mouseleave", handleMouseLeave);
+      document.documentElement.removeEventListener("mouseleave", handleMouseLeave);
+      document.documentElement.removeEventListener("mouseenter", handleMouseEnter);
     };
-  }, []);
+  }, [isVisible]);
 
-  // Check if the cursor is outside the viewport
-  const isOutsideViewport =
-    position.x < 0 ||
-    position.x > window.innerWidth ||
-    position.y < 0 ||
-    position.y > window.innerHeight;
-
-  // If it's a mobile device or if the cursor is outside the viewport, don't render the custom cursor
-  if (isOutsideViewport || !isVisible) {
+  if (!isVisible) {
     return null;
   }
 
   return (
     <div
       className="custom-cursor"
-      style={{ left: `${position.x}px`, top: `${position.y}px` }}
+      style={{ 
+        left: `${position.x}px`, 
+        top: `${position.y}px`,
+        pointerEvents: "none"
+      }}
     ></div>
   );
 };
